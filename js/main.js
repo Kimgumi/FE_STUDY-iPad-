d4b999079451ae0dc9ad50eadc3d608201d00c03
Kimgumi <apple220241@gmail.com>
2025-05-27 14:53:45 +0900

    검색바 스크립트01

--- js/main.js
+++ js/main.js
@@ -6,21 +6,44 @@ basketStarterEL.addEventListener('click', function(event){
   // basketStarter 영역을 클릭했을 때
   if (basketEL.classList.contains('show')){ // contains는 존재하는지 T/F로 반환
     // hide인 상태
-    basketEL.classList.remove('show'); // show 클래스를 제거
+    hideBasket(); // show 클래스를 제거
   } else {
     // show인 상태
-    basketEL.classList.add('show'); // show 클래스를 추가
+    showBasket();// show 클래스를 추가
   }
 });
 basketEL.addEventListener('click', function(event){
   event.stopPropagation(); // 이벤트 전파를 막음(버블링 방지)
-  // basket 영역을 클릭했을 때
-  // 아무것도 하지 않음 => 외부 영역을 눌러도 영향X
+  // basket 영역을 클릭했을 때 아무것도 하지 않음 => 외부 영역을 눌러도 영향X
 });
 window.addEventListener('click', function(){
   basketEL.classList.remove('show'); 
   // show 클래스를 제거 => 외부 영역을 눌러도 영향X
 });
 
+const headerEL = document.querySelector('header');
+const searchWrapEL = headerEL.querySelector('.search-wrap');
+const searchStarterEL = headerEL.querySelector('.search-starter');
+const searchCloserEL = searchWrapEL.querySelector('.search-closer');
+const searchShadowEL = searchWrapEL.querySelector('.shadow');
+
+searchStarterEL.addEventListener('click',function(){
+  showSearch(); // show 클래스를 추가
+})
+searchCloserEL.addEventListener('click',function(){
+  hideSearch(); // show 클래스를 제거
+})
+searchShadowEL.addEventListener('click',function(){
+  hideSearch(); // show 클래스를 제거
+})
+
 
 // 추상화 
+function showBasket () {basketEL.classList.add('show')}; 
+function hideBasket () {basketEL.classList.remove('show')}; 
+function showSearch () {
+  headerEL.classList.add('searching')
+}; 
+function hideSearch () {
+  headerEL.classList.remove('searching')
+}; 
