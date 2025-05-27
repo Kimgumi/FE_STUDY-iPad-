1235f3d645850ae3ab21abd4d1dabfdb0bf4bf84
Kimgumi <apple220241@gmail.com>
2025-05-27 11:30:25 +0900

    장바구니 애니메이션 처리

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const basketStarterEL = document.querySelector('header .basket-starter');
+const basketEL = basketStarterEL.querySelector('header .basket');
+
+basketStarterEL.addEventListener('click', function(event){
+  event.stopPropagation(); // 이벤트 전파를 막음(버블링 방지)
+  // basketStarter 영역을 클릭했을 때
+  if (basketEL.classList.contains('show')){ // contains는 존재하는지 T/F로 반환
+    // hide인 상태
+    basketEL.classList.remove('show'); // show 클래스를 제거
+  } else {
+    // show인 상태
+    basketEL.classList.add('show'); // show 클래스를 추가
+  }
+});
+basketEL.addEventListener('click', function(event){
+  event.stopPropagation(); // 이벤트 전파를 막음(버블링 방지)
+  // basket 영역을 클릭했을 때
+  // 아무것도 하지 않음 => 외부 영역을 눌러도 영향X
+});
+window.addEventListener('click', function(){
+  basketEL.classList.remove('show'); 
+  // show 클래스를 제거 => 외부 영역을 눌러도 영향X
+});
+
+
+// 추상화 
